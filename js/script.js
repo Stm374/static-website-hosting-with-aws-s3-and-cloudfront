3fc57e6d4fd1edb19298d9765bc188b276f47340
Satyam <sinhasatyam2805@gmail.com>
2025-12-07 02:21:36 +0530

    js commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Simple JavaScript to show an alert when page loads
+window.onload = function () {
+    console.log("Website loaded successfully!");
+
+    // Example interactive message
+    const message = "Welcome to my Cloud-Hosted Website!";
+    alert(message);
+};
+
+// Example function for future use
+function showMessage() {
+    alert("You clicked a button!");
+}
